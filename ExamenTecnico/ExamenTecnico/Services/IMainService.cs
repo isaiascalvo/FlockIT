8558d26a58ed8e1b7b5ad3905c2cb65969fabed1
Isaías Calvo <iscalv@moravia.com>
2021-05-22 12:46:56 -0300

    Creating the project

---
+++ ExamenTecnico/ExamenTecnico/Services/IMainService.cs
@@ -0,0 +1,14 @@
+﻿using ExamenTecnico.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExamenTecnico.Services
+{
+    public interface IMainService
+    {
+        Task<bool> Login(Login login);
+        Task<ProvData> GetProvByName(string provName);
+    }
+}
---
+++ ExamenTecnico/ExamenTecnico/Services/MainService.cs
@@ -0,0 +1,29 @@
+﻿using ExamenTecnico.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ExamenTecnico.Services
+{
+    public class MainService : IMainService
+    {
+        public async Task<ProvData> GetProvByName(string provName)
+        {
+            var client = new HttpClient();
+            var provData = new ProvData()
+            {
+                Long = 1,
+                Lat = 1
+            };
+
+            return provData;
+        }
+
+        public async Task<bool> Login(Login login)
+        {
+            return login.Username == "isaías" && login.Password == "isaías";
+        }
+    }
+}
