5d12d8a2cec645a19a99ab17135f929f1a16ec39
Isaías Calvo <iscalv@moravia.com>
2021-05-24 17:01:17 -0300

    Modifing the Readme file

--- ExamenTecnico/Tests/Tests.cs
+++ ExamenTecnico/Tests/Tests.cs
@@ -15,25 +15,50 @@ namespace Tests
             _mainService = new MainService();
         }
 
+
+        //Los comentarios colocados en los tests son para indicar que hubiera hecho si tuviese una capa de accesso a datos
+
         [Fact]
         public async Task LoginSuccessfullyTest()
         {
+            //Arrange
             var login = new Login { Username = "isaías", Password = "isaías" };
+            
+            //_logginRepository.Setup(l => l.Login(It.Any<Login>())).ReturnAsync(new User
+            //{
+            //    Id = 1,
+            //    Username = "isaías",
+            //    FirstName = "Isaías",
+            //    LastName = "Calvo"
+            //});
+
+            //Act
             var result = await _mainService.Login(login);
 
+            //Assert
             Assert.NotNull(result);
             Assert.NotNull(result.Username);
             Assert.NotNull(result.FirstName);
             Assert.NotNull(result.LastName);
+
+            //_logginRepository.Verify(x => x.Login(It.Any<Login>()), Times.Once);
         }
 
         [Fact]
         public async Task LoginFailTest()
         {
+            //Arrange
             var login = new Login { Username = "asdasd", Password = "asdasd" };
+
+            //_logginRepository.Setup(l => l.Login(It.Any<Login>())).ReturnAsync((User) null);
+
+            //Act
             var result = await _mainService.Login(login);
 
+            //Assert
             Assert.Null(result);
+
+            //_logginRepository.Verify(x => x.Login(It.Any<Login>()), Times.Once);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # FlockIT
 Examen Técnico
+
+Aclaraciones, suposiciones y consideraciones:
+- En cada llamada a alguno de los endpoints se devuelve la siguiente estructura:
+    {
+        isSuccessfully: indica si la operación se realizó con éxito o no
+        result: en caso de que la operación se realice con éxito devuelve un resultado, caso contrario null
+        errorMessage: en caso de que la operación se realice sin éxito devuelve un mensaje de error, caso contrario null
+    }
+- Se utilizó Nlog para mantener logs de la aplición en archivos. Los archivos de log serán almacenados en la ruta "\bin\Debug\net5.0\logs" dentro de la carpeta contenedora del proyecto.
+- Ya que los tests que hice me parecían muy sonsos, dejé código comentado para indicar que hubiera hecho si tuviese una capa de accesso a datos.
